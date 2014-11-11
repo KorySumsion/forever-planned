3e90ea264e2f820302fa301a203c079d20600e83
Mark Tonkinson <DHTMaster@gmail.com>
2014-11-11 13:58:43 -0700

    the auth endpoints were created, but I am refactoring to use passport.

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
+"use-strict"
 var Express = require('express');
 var BodyParser = require('body-parser');
+var Passport = require('passport');
+var Session = require('express-session');
 var Mongoose = require('mongoose');
+var Schema = Mongoose.Schema;
+var mongoUri = 'mongodb://localhost:27017/WeddingPlans';
+
+Mongoose.connect(mongoUri);
+var connection = Mongoose.connection;
+connection.once('open', function(){
+	console.log('mongo listening on ' + mongoUri);
+})
 
 var app = Express();
 
@@ -8,12 +19,15 @@ var port = 9999;
 
 app.use(Express.static(__dirname + '/Public'))
 app.use(BodyParser.json());
+app.use(Session ({ secret: 'wedding secrets'}));
+app.use(Passport.initialize());
+app.use(Passport.session());
 
 var AuthController = require('./lib/controllers/auth-controller');
 
 /*Authorization Routes*/
-app.post('/api/auth', AuthController.authorize);
-
+app.post('/api/login', AuthController.login);
+app.post('/api/newUser', AuthController.createUser)
 
 
 
