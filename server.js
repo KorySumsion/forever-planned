b58d9bfcdeed6d890b8704541916e764aa0ea357
Mark Tonkinson <DHTMaster@gmail.com>
2014-11-11 14:35:17 -0700

    please work

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var BodyParser = require('body-parser');
 var Passport = require('passport');
 var Session = require('express-session');
 var Mongoose = require('mongoose');
+var LocalStrategy = require('passport-local').Strategy;
 var Schema = Mongoose.Schema;
 var mongoUri = 'mongodb://localhost:27017/WeddingPlans';
 
@@ -23,10 +24,44 @@ app.use(Session ({ secret: 'wedding secrets'}));
 app.use(Passport.initialize());
 app.use(Passport.session());
 
+/* Controllers for Routes*/
 var AuthController = require('./lib/controllers/auth-controller');
 
+
+/* User Model Reference for Passport*/
+
+//var User = require(usermodelfile)
+
+Passport.serializeUser(function(user, done) {
+  
+  done(null, user);
+});
+
+Passport.deserializeUser(function(user, done) {
+
+  done(null, user);
+});
+
+
+
+Passport.use(new LocalStrategy(
+  function(req.user.email, req.user.password, done) {
+    User.findOne({ email: user.email }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
+));
+
+
 /*Authorization Routes*/
-app.post('/api/login', AuthController.login);
+app.post('/api/login', Passport.authenticate('local'), AuthController.login);
 app.post('/api/newUser', AuthController.createUser)
 
 
