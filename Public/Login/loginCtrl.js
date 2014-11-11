2cd2d655ee8615ec3c45cb174cd8f305170d22b6
kweek <kdweekley@gmail.com>
2014-11-11 11:25:13 -0700

    Krissy's Login Updates

--- Public/Login/loginCtrl.js
+++ Public/Login/loginCtrl.js
@@ -1,1 +1,14 @@
-loginCtrl.js
+var app = angular.module('wedding');
+
+app.controller('signupCtrl', function($scope, $cookieStore){
+	
+	$scope.login = function(){
+		authService.loginUser($scope.user).then(function(res){
+			if(res.errorMessage){
+				$scope.error = res.errorMessage;
+			} else {
+				$location.path('/')
+			}
+		})
+		
+	}
