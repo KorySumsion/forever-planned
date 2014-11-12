839fa1223ef11aee814c1176d35c5e810e384e13
KorySumsion <korysumsion@gmail.com>
2014-11-12 16:39:09 -0700

    Kory's Sweet Setup

--- Public/setup/setupController.js
+++ Public/setup/setupController.js
@@ -1,5 +1,21 @@
 var app = angular.module('wedding');
 
-app.controller('setupController', function($scope){
+app.controller('setupController', function($scope, $timeout){
 	$scope.step1 = false;
+	$scope.step2 = false;
+
+	$scope.showStep1 = function(){
+		$scope.step1 = true;
+	}
+
+	$scope.showStep2 = function(){
+		$scope.step1 = false;
+		$timeout(function(){
+			$scope.step2 = true;}, 500);
+	}
+	$scope.goToNames = function(){
+		$scope.step2 = false;
+		$timeout(function(){
+			$scope.step1 = true;}, 500);
+	}
 })
