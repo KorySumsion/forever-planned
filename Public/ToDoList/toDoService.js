5ecf129b052d05273f909df6e8a78675b60ca18b
Mark Tonkinson <DHTMaster@gmail.com>
2014-11-13 16:58:38 -0700

    To do is functioning

--- Public/ToDoList/toDoService.js
+++ Public/ToDoList/toDoService.js
@@ -16,10 +16,11 @@ app.service('toDoService', function($http, $q){
 	}
 
 	this.editTodos = function(userObj){
+		
 		var deferred = $q.defer();
 		$http({
 			method: 'PUT',
-			url: '/api/updateUser' + userObj._id,
+			url: '/api/updateUser/' + userObj._id,
 			data: userObj
 		}).then(function(res){
 			return deferred.resolve(res.data);
