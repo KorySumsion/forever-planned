d808c066602d793b6925af0fa0bbaece586d2cff
KorySumsion <korysumsion@gmail.com>
2014-11-17 15:15:16 -0700

    kory's changes

--- Public/Budget/budgetCtrl.js
+++ Public/Budget/budgetCtrl.js
@@ -2,8 +2,16 @@ var app = angular.module('wedding');
 
 app.controller('budgetCtrl', function($scope){
 
-	$scope.budget = "$" + $scope.currentUser.budget;
-
-
+	// $scope.budget = "$" + $scope.currentUser.budget;
+	// var updateBudget = function(){
+	// 	if($scope.currentUser.ideas){
+	// 		var items = $scope.currentUser.ideas;
+	// 		for (var i = 0; i < items.length; i++){
+	// 			var ideas = items[i].boardItems;
+	// 		}
+	// 		for (var)
+	// 	}
+	// }
+	// updateBudget()
 
 })
