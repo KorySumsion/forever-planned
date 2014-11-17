9b5ee1241f9f1d4228eab5d6da0420db89dcf042
Mark Tonkinson <DHTMaster@gmail.com>
2014-11-17 13:54:05 -0700

    Trying to fix the home page when it gets bigger and smaller

--- Public/Banner/bannerCtrl.js
+++ Public/Banner/bannerCtrl.js
@@ -1,7 +1,19 @@
 var app = angular.module('wedding');
 
-app.controller('bannerCtrl', function($scope, $cookieStore){
+app.controller('bannerCtrl', function($scope, $cookieStore, $window){
 	$scope.logout = function(){
 		$cookieStore.remove('currentUser');
 	}
+
+  	var w = angular.element($window);
+  
+ 	// w.bind('resize', function() {
+  //      	if($window.innerWidth > 1000){
+		// 	$scope.weirdMargins = "weird-margin-issues"
+		// } else if($window.innerWidth < 1000){
+		// 	$scope.weirdMargins = "weird-margin-issues-two"
+		// }
+  // 	});
+
+	
 })
