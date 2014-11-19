d603569884d2ce71be24456c5ff7178e2867be6f
Mark Tonkinson <DHTMaster@gmail.com>
2014-11-19 15:18:09 -0700

    I'm so fancy

--- Public/Login/loginCtrl.js
+++ Public/Login/loginCtrl.js
@@ -4,7 +4,7 @@ var app = angular.module('wedding');
 
 app.controller('loginCtrl', function($scope, $state, authService){
 
-
+	$scope.showLogin = true;
 	$scope.login = function(){
 		
 
@@ -12,6 +12,17 @@ app.controller('loginCtrl', function($scope, $state, authService){
 				$state.go('auth.home', {userid: res._id});
 		})
 		
+	}
+
+	$scope.showSignup = false;
+	$scope.displaySignup = function(){
+		$scope.showSignup = true;
+		$scope.showLogin = false;
+	}
+
+	$scope.displayLogin = function(){
+		$scope.showSignup = false;
+		$scope.showLogin = true;
 	}
 })
 
